272445d3b729ae60d5cbcc6e09585379a1214a14
unknown <vishuthedj@gmail.com>
2017-09-04 13:21:26 +0530

    First Commit

---
+++ src/main/java/com/mindfire/download/DownloadServlet.java
@@ -0,0 +1,50 @@
+package com.mindfire.download;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Vishal Prasadd
+ */
+//@WebServlet(urlPatterns = {"/DownloadServlet"})
+@WebServlet(name = "DownloadServlet", urlPatterns = {"/DownloadServlet"})
+
+public class DownloadServlet extends HttpServlet {
+
+  
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String filename = "image.png";          //File to be Downloaded
+        String filepath = "C:/Users/Vishal Prasad/Desktop/"; //Path of the File form where to be downloaded
+        response.setContentType("APPLICATION/OCTET-STREAM");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+
+        FileInputStream fileInputStream = new FileInputStream(filepath + filename);
+
+        int i;
+        while ((i = fileInputStream.read()) != -1) {
+            out.write(i);
+        }
+        fileInputStream.close();
+        out.close();
+    }
+
+ 
+
+}
